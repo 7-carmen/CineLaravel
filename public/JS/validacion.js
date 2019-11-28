07139892831e19cfc08034c47de566eefceb1ba1
Carmen Soriano <carmen.sorianotortosa@gmail.com>
2019-11-28 19:37:12 +0100

    Arreglada validación de formulario y mostrar video

--- public/JS/validacion.js
+++ public/JS/validacion.js
@@ -1,42 +1,45 @@
 const validacion = () => {
     const aviso = `Debe rellenar este campo.`;
+    let enviar = true;
         //Nombre:
          var nombre = document.getElementById("nombre");
         if (nombre.value == "") {
             document.getElementById("e_nombre").innerHTML = aviso;
-            return false;
+            enviar = false;
         }else{
             document.getElementById("e_nombre").innerHTML = "";
         } 
         //Duración:
         var duracion = document.getElementById("duracion");
         var v_duracion = /^\d+$/;
+        console.log("Duración: "+duracion);
         if(duracion.value==""){
-            ducument.getElementById("e_duracion").innerHTML = aviso;
-            return false;
+            document.getElementById("e_duracion").innerHTML = aviso;
+            console.log("hola");
+            enviar = false;
         }else if(!v_duracion.test(duracion.value)){
-            ducument.getElementById("e_duracion").innerHTML = "Debes de introducir caracteres numericos";
-            return false;
+            document.getElementById("e_duracion").innerHTML = "Debes de introducir caracteres numericos";
+            enviar = false;
         }else{
-            ducument.getElementById("e_duracion").innerHTML = "";
+            document.getElementById("e_duracion").innerHTML = "";
         }
         //Año
         var año = document.getElementById("anyo");
          var re_año = /^\d{4}$/;
         if (año.value == "") {
             document.getElementById("e_anyo").innerHTML = aviso;
-            return false;
+            enviar = false;
         } else if (!re_año.test(año.value)) {
             document.getElementById("e_anyo").innerHTML = "Escriba el año en formato \"0000\"";
-            return false;
+            enviar = false;
         }else{ 
             document.getElementById("e_anyo").innerHTML = "";
         }
         //Puntuación
-         var puntuacion = document.getElementById("puntuacion");
+         var puntuacion = document.getElementById("rating");
         if (puntuacion.value == "") {
             document.getElementById("e_puntuacion").innerHTML = aviso;
-            return false;
+            enviar = false;
         }else{
             document.getElementById("e_puntuacion").innerHTML = "";
         } 
@@ -44,7 +47,7 @@ const validacion = () => {
          var sinopsis = document.getElementById("sinopsis");
         if (sinopsis.value == "") {
             document.getElementById("e_sinopsis").innerHTML = aviso;
-            return false;
+            enviar = false;
         }else{
             document.getElementById("e_sinopsis").innerHTML = "";
         } 
@@ -52,8 +55,10 @@ const validacion = () => {
           var cartel = document.getElementById("cartel");
         if (cartel.value == "") {
             document.getElementById("e_cartel").innerHTML = aviso;
-            return false;
+            enviar = false;
         }else{
             document.getElementById("e_cartel").innerHTML = "";
         } 
+
+        return enviar;
     }
--- resources/views/ver.blade.php
+++ resources/views/ver.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master') <!--Extiende desde master que contiene la cabecera.-->
 @section('contenido') <!--Dentro se coloca el contenido que se incrustará en el master. -->
-<video width="600" height="400">
+<video width="600" height="400" controls>
     <source src="/Videos/prueba.mp4" type="video/mp4">
 </video>
 @endsection
